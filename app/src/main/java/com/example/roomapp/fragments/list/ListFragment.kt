fef95ef15c759207bbe85cbcf31c18dc8a519002
Filipe Pimentel Pinto <filipepimentel.fp@gmail.com>
2023-05-01 17:10:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
@@ -0,0 +1,90 @@
+package com.example.roomapp.fragments.list
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomapp.R
+import com.example.roomapp.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
+    val adapter = ListAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // Recyclerview
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        view.et_search.addTextChangedListener {
+            searchDatabase(it.toString())
+        }
+
+        // Add menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_delete -> { deleteAllUsers() }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun searchDatabase(query: String) {
+        val searchQuery = "%$query%"
+
+        Toast.makeText(requireContext(), "$searchQuery", Toast.LENGTH_SHORT).show()
+
+        mUserViewModel.searchDatabase(searchQuery).observe(viewLifecycleOwner) { users ->
+            adapter.setData(users)
+        }
+    }
+
+    private fun deleteAllUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteAllUsers()
+            Toast.makeText(
+                requireContext(),
+                "Successfully removed everything",
+                Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure you want to delete everything?")
+        builder.create().show()
+    }
+}
